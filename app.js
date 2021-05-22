1d61ad7e1e2108cc7ee70935ba6ade0e3db045ca
Aman gupta <amangupta.95@outlook.com>
2021-05-22 12:41:48 +0530

    fixed button active state

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 let isShowing = true;
 const sharePopup = document.getElementById('share-popup');
-document.getElementById('share').addEventListener('click', () => {
+const shareBtn = document.getElementById('share');
+shareBtn.addEventListener('click', () => {
 	sharePopup.classList.remove('dont-show');
 	if (isShowing) {
 		isShowing = false;
 		sharePopup.classList.remove('deactivate-share-popup');
 		sharePopup.classList.add('activate-share-popup');
+		shareBtn.classList.add('share-btn-active');
 	} else {
 		isShowing = true;
 		sharePopup.classList.remove('activate-share-popup');
 		sharePopup.classList.add('deactivate-share-popup');
+		shareBtn.classList.remove('share-btn-active');
 	}
 });
